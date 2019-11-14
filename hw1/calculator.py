9552338a7b17ac9308b33c1914a3b21ff1151e7f
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-11-14 12:25:30 +0300

    Add calculator stub

---
+++ hw1/calculator.py
@@ -0,0 +1,31 @@
+def add(x : int, y : int) -> int:
+    pass
+
+
+def sub(x : int, y : int) -> int:
+    pass
+
+
+def mult(x : int, y : int) -> int:
+    pass
+
+
+def div(x : int, y : int) -> int:
+    pass
+
+
+def mod(x : int, y : int) -> int:
+    pass
+
+
+def pow(x : int, y : int) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    print(add(4, 5))
+    print(sub(3, 1))
+    print(mult(2, 2))
+    print(div(4, 2))
+    print(mod(5, 3))
+    print(pow(6, 2))
