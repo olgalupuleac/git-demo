e53d59f8bd8b4bd6c2a93b2e1787dad5cf64e81c
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-11-14 12:30:25 +0300

    Add strings stub

---
+++ hw2/strings.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def filter_lines_of_length_five(lines: List[str]) -> List[str]:
+    pass
+
+
+if __name__ == '__main__':
+    print(filter_lines_of_length_five(['aaa', 'ab', 'abaab', 'ccccc']))
